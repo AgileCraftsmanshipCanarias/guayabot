bf203ec8be9775dbb63cee3d5d7ba9b119693e23
Daniel Ramos <danielramosacosta1@gmail.com>
2025-06-08 12:44:29 +0200

    update message

--- bot.ts
+++ bot.ts
@@ -132,7 +132,7 @@ function getWelcomeMessage(names: string[]) {
   const formattedNames = formatNamesList(names);
   const [te, puedes] = names.length > 1 ? ["Les", "Pueden"] : ["Te", "Puedes"];
 
-  return `¡Muy buenas, ${formattedNames}! 🌞 ${te} damos la bienvenida con cariño isleño al grupo de Agile Canarias en Telegram. Aquí ${puedes} ver las normas del grupo:`;
+  return `¡Muy buenas, ${formattedNames}! 🌞 ${te} damos la bienvenida con cariño isleño al grupo de Agile Canarias en Telegram. Aquí ${puedes} ver las normas del grupo:\nhttps://t.me/AgileGuayotaBot?start=rules`;
 }
 
 export async function onSessionReceived(
